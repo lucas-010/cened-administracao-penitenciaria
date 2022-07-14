4bdd70c466226fb626462134dcba4556c804913d
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-14 17:15:10 -0300

    404 fixed

--- src/Components/Manager/Header.jsx
+++ src/Components/Manager/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BiBookReader} from 'react-icons/bi'
+import {FaBookReader} from 'react-icons/fa'
 
 export default function Header() {
   return (
-    <div className='bg-[#594ae1] w-full p-2'>
+    <div className='bg-[#594ae1] w-full p-4'>
         <div className='text-white flex items-center text-xl'>
-            <BiBookReader size={40}/>
+            <FaBookReader size={30}/>
             <h1 className='ml-2'>CENED Qualificando</h1>
         </div>
     </div>
