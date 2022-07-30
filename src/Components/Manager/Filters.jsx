afa5622e380452cd3a38f640a276b47364328026
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-30 10:49:38 -0300

    filters and search concludeds

--- src/Components/Manager/Filters.jsx
+++ src/Components/Manager/Filters.jsx
@@ -6,37 +6,32 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import {MdFilterAlt} from 'react-icons/md'
 import {AiFillPrinter} from 'react-icons/ai'
 import React, { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 
-export default function Filters() {
-    const [dateRegistration1, setDateRegistration1] = useState('')
-    const [dateRegistration2, setDateRegistration2] = useState('')
-    const [dateRegistration3, setDateRegistration3] = useState('')
-    const [dateRegistration4, setDateRegistration4] = useState('')
+export default function Filters({searchEnter, filterData}) {
+    const [date1, setDate1] = useState('')
+    const [date2, setDate2] = useState('')
+    const [date3, setDate3] = useState('')
+    const [date4, setDate4] = useState('')
+    const [statusCourse, setStatusCourse] = useState(0)
+    const [searchValue, setSearchValue] = useState('')
   return (
     <div className='m-10'>
         <Grid container spacing={2}>
             <Grid item xs={10} md={7}>
-                <TextField className='w-full' label='Digite o Nome da Penitenciária' select>
-                    <MenuItem value={1}>
-                        Penitenciária 1
-                    </MenuItem>
-
-                    <MenuItem value={2}>
-                        Penitenciária 2
-                    </MenuItem>
-
-                    <MenuItem value={3}>
-                        Penitenciária 3
-                    </MenuItem>
-
-                    <MenuItem value={4}>
-                        Penitenciária 4
+                <TextField select label='Selecione a Penitenciária' className='w-full'>
+                    <MenuItem value={45}>
+                        Penitenciária Aruana
                     </MenuItem>
                 </TextField>
             </Grid>
 
             <Grid item xs={10} md={4}>
-                <TextField className='w-full' label='Situação do Curso' select>
+                <TextField value={statusCourse} onChange={(e)=> setStatusCourse(e.target.value)} className='w-full' label='Situação do Curso' select>
+                    <MenuItem value={0}>
+                        Todos
+                    </MenuItem>
                     <MenuItem value={1}>
                         Aguardando Pagamento
                     </MenuItem>
@@ -66,33 +61,26 @@ export default function Filters() {
                     </MenuItem>
                 </TextField>
             </Grid>
-
+            <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
             <Grid item xs={10} md={1.5}>
-                <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDateRegistration1(e)} label='Matrículas (a partir)' value={dateRegistration1} renderInput={(props)=> <TextField {...props}/>}/>
-                </LocalizationProvider>
+                <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDate1(e)} label='Matrículas (a partir)' value={date1} renderInput={(props)=> <TextField {...props}/>}/>
             </Grid>
 
             <Grid item xs={10} md={1.5}>
-                <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDateRegistration2(e)} label='Matrículas (até)' value={dateRegistration2} renderInput={(props)=> <TextField {...props}/>}/>
-                </LocalizationProvider>
+                <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDate2(e)} label='Matrículas (até)' value={date2} renderInput={(props)=> <TextField {...props}/>}/>
             </Grid>
 
             <Grid item xs={10} md={1.5}>
-                <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDateRegistration3(e)} label='Início (a partir)' value={dateRegistration3} renderInput={(props)=> <TextField {...props}/>}/>
-                </LocalizationProvider>
+                <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDate3(e)} label='Início (a partir)' value={date3} renderInput={(props)=> <TextField {...props}/>}/>
             </Grid>
 
             <Grid item xs={10} md={1.5}>
-                <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDateRegistration4(e)} label='Início (até)' value={dateRegistration4} renderInput={(props)=> <TextField {...props}/>}/>
-                </LocalizationProvider>
+                <DesktopDatePicker className='w-full' inputFormat="dd/MM/yyyy" onChange={(e)=> setDate4(e)} label='Início (até)' value={date4} renderInput={(props)=> <TextField {...props}/>}/>
             </Grid>
+            </LocalizationProvider>
 
             <Grid item xs={10}  md={1}>
-                <button className='text-lg font-bold w-full transition-colors mt-2 hover:bg-[#ff1b67] hover:shadow-xl text-white rounded-md justify-center p-2 shadow-lg flex items-center bg-[#ff4181]'><MdFilterAlt size={20}/>FILTRAR</button>
+                <button onClick={()=> {filterData(date1, date2, date3, date4, statusCourse); setSearchValue('')}} className='text-lg font-bold w-full transition-colors mt-2 hover:bg-[#ff1b67] hover:shadow-xl text-white rounded-md justify-center p-2 shadow-lg flex items-center bg-[#ff4181]'><MdFilterAlt size={20}/>FILTRAR</button>
             </Grid>
 
             <Grid item xs={10}  md={2}>
@@ -100,7 +88,7 @@ export default function Filters() {
             </Grid>
 
             <Grid item xs={10} md={11}>
-                <TextField label='Pesquisar por Estudante ou Curso' placeholder='Digite o Nome do Estudante ou o Curso (pressione "Enter" para pesquisar)' className='w-full'/>
+                <TextField variant='standard' onKeyUp={(e)=> e.code === 'Enter' ? searchEnter(searchValue) : false} value={searchValue} onChange={(e)=> setSearchValue(e.target.value)} label='Pesquisar por Estudante ou Curso' placeholder='Digite o Nome do Estudante ou o Curso (pressione "Enter" para pesquisar)' className='w-full'/>
             </Grid>
         </Grid>
     </div>
--- src/Pages/Manager.jsx
+++ src/Pages/Manager.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Filters from '../Components/Manager/Filters'
 import Header from '../Components/Manager/Header'
 import Table from '../Components/Manager/TableContent'
 import Title from '../Components/Manager/Title'
 
 export default function Manager() {
+  const [searchValueChange, setSearchValueChange] = useState('')
+  const [statusCourseChange, setStatusCourseChange] = useState('')
+  const [date1, setDate1] = useState('')
+  const [date2, setDate2] = useState('')
+  const [date3, setDate3] = useState('')
+  const [date4, setDate4] = useState('')
+  const searchEnter = (value)=>{
+    setSearchValueChange(value)
+    setDate1('')
+    setDate2('')
+    setDate3('')
+    setDate4('')
+  }
+  const filterData = (d1, d2, d3, d4, statusCourse)=>{
+    d1 !== '' ? setDate1(new Date(d1).toLocaleDateString('en-us')) : setDate1('')
+    d2 !== '' ? setDate2(new Date(d2).toLocaleDateString('en-us')) : setDate2('')
+    d3 !== '' ? setDate3(new Date(d3).toLocaleDateString('en-us')) : setDate3('')
+    d4 !== '' ? setDate4(new Date(d4).toLocaleDateString('en-us')) : setDate4('')
+    setSearchValueChange('')
+    setStatusCourseChange(statusCourse)
+  }
   return (
     <div className=''>
         <Header/>
         <Title/>
-        <Filters/>
-        <Table/>
+        <Filters filterData={filterData} searchEnter={searchEnter}/>
+        <Table statusCourse={statusCourseChange} date1={date1} date2={date2} date3={date3} date4={date4} searchValueChange={searchValueChange}/>
     </div>
   )
 }
